2add36435164618586aaab6ab475bdf62fd63c47
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-03-03 15:35:22 +0100

    Move to bootstrap 2 and add OS X pages and links

--- js/application.js
+++ js/application.js
@@ -1,43 +1,4 @@
 jQuery(document).ready(function($){
 
-  $('#github').mouseover(function(){
-    $('#mousing').text("GitHub");
-  });
-
-  $('#github').mouseout(function(){
-    $('#mousing').html("&nbsp;");
-  });
-
-  $('#twitter').mouseover(function(){
-    $('#mousing').text("Twitter");
-  });
-
-  $('#twitter').mouseout(function(){
-    $('#mousing').html("&nbsp;");
-  });
-
-  $('#rss').mouseover(function(){
-    $('#mousing').text("RSS");
-  });
-
-  $('#rss').mouseout(function(){
-    $('#mousing').html("&nbsp;");
-  });
-
-// dropdown menu
-$("body").bind("click", function (e) {
-  $('a.menu').parent("li").removeClass("open");
-});
-
-$("a.menu").click(function (e) {
-  var $li = $(this).parent("li").toggleClass('open');
-  return false;
-});
-
-$(window).load(function() {
-  $('#featured').orbit({
-	bullets: true
-  });
-});
 
 });
